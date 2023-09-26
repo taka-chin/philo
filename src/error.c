6f00cb957be81c5f23ad10d62904dd57e5258a33
taka-chin <you@example.com>
2023-09-26 16:38:04 +0900

    add input check

--- src/error.c
+++ src/error.c
@@ -3,7 +3,10 @@
 void ft_put_error(int e_error_type)
 {
 	if(e_error_type == ARGS_ERROR)
+	{
 		printf("invaild args\n");
+		exit(1);
+	}
 	else if(e_error_type == MALLOC_ERROR)
 		printf("PTHREAD_ERROR\n");
 	else if(e_error_type == PTHREAD_ERROR)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include "philo.h"
 
+static void	input_check(char **input)
+{
+	const char	*str;
+
+	while (*++input)
+	{
+		str = *input;
+		if (str[0] == '+' || str[0] == '-')
+			str++;
+		while (*str)
+		{
+			if (!ft_isdigit(*str))
+				ft_put_error(ARGS_ERROR);
+			str++;
+		}
+	}
+}
+
 int main(int argc,char *argv[])
 {
 	t_args *input;
@@ -8,6 +26,7 @@ int main(int argc,char *argv[])
 
 	if(argc < 5 || argc >= 7)
 		ft_put_error(ARGS_ERROR);
+	input_check(argv);
 	input = init_args(argc, argv);
 	fork = init_fork(input);
 	philo = init_philo(input,fork);
